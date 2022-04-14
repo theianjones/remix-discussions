db9eb8081cec05f903531e72213ad34c55938d3e
Ian Jones <ian@egghead.io>
2022-04-14 09:46:30 -0400

    planescalegit add .

--- app/routes/__app/posts.tsx
+++ app/routes/__app/posts.tsx
@@ -1,9 +1,18 @@
-import {Outlet} from 'remix'
+import {Link, Outlet} from 'remix'
 
 export default function Posts() {
   return (
     <div>
-      <h1>Posts</h1>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <h1>Posts</h1>
+        <Link to="/posts/new">New Post</Link>
+      </div>
       <Outlet />
     </div>
   )
